58f07e48fa02e695f349e279754610a5baea0ed5
avitaind <52917489+avitaind@users.noreply.github.com>
2019-11-22 19:10:48 +0530

    Update map.blade.php

--- public/resources/views/product/map.blade.php
+++ public/resources/views/product/map.blade.php
@@ -303,6 +303,5 @@
 
 
   </script>
-	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDkjCWGYO6wojvQEwcNm09P_H7XeYoji9I&callback=initMap"></script>
-
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWQCKIl7B4w27KcSW-tW4ja_Rk4SbcPnk&callback=initMap">  </script>
 @endsection
