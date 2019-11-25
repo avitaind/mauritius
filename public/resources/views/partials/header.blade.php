c0395503a184b4383a9112242d32cf41b8415674
avitaind <52917489+avitaind@users.noreply.github.com>
2019-11-25 10:26:39 +0530

    Update header.blade.php

--- public/resources/views/partials/header.blade.php
+++ public/resources/views/partials/header.blade.php
@@ -98,8 +98,8 @@
                             </li>
                             
                             <li class="nav-item has-dropdown">
-                                <input id="header_product_device" type="checkbox" hidden="">
-                                <a class="nav-link px-md-4 py-2"><label for="header_product_device" class="d-block mb-0">@lang('site.accessories')</label></a>
+                                <input id="header_product_accessories" type="checkbox" hidden="">
+                                <a class="nav-link px-md-4 py-2"><label for="header_product_accessories" class="d-block mb-0">@lang('site.accessories')</label></a>
                                 <div class="dropdown">
                                     <ul class="list-unstyled">
 
