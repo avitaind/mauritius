4a6bffc5ecdee0e163135f36e6899f743ec022a3
avitaind <52917489+avitaind@users.noreply.github.com>
2020-03-06 11:42:13 +0530

    Update support.blade.php

--- public/resources/views/product/support.blade.php
+++ public/resources/views/product/support.blade.php
@@ -211,9 +211,9 @@
 
 
 	</script>
- 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-nJUpMSCzA84jIss1RPTkzZSS3Sz1thc&callback=initMap">  </script>
+ <!---	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-nJUpMSCzA84jIss1RPTkzZSS3Sz1thc&callback=initMap">  </script> 
 	
-    <script src="{{asset('/js/support.js')}}"></script>
+    <script src="{{asset('/js/support.js')}}"></script> --->
 
 
 @endsection
